54b4f32b372e2c1d7bc684049d5fc1cb136ead3c
TzuHuang Yen <yan0912@hotmail.com>
2022-08-30 11:01:40 +0200

    Update 2022-08-30T09:01:30.584Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,5 +1,6 @@
-//cdn.jsdelivr.net/npm/vanillajs-datepicker@1.2.0/dist/js/datepicker-full.min.js"
+"use strict";
 
+//cdn.jsdelivr.net/npm/vanillajs-datepicker@1.2.0/dist/js/datepicker-full.min.js"
 https: $(function () {
   console.log("Hello Bootstrap5");
 });
@@ -8,30 +9,28 @@ $(".menuBtn").click(function (e) {
   e.preventDefault();
   $(".dropdown").toggleClass("active");
 });
-
-const elem = document.querySelector('input[name="datepicker"]');
-const datepicker = new Datepicker(elem, {
-  autohide: true,
-});
-
-//index footer swiper
+var elem = document.querySelector('input[name="datepicker"]');
+var datepicker = new Datepicker(elem, {
+  autohide: true
+}); //index footer swiper
 // <!-- Initialize Swiper -->
 
-const swiper = new Swiper(".swiper", {
+var swiper = new Swiper(".swiper", {
   effect: "fade",
   loop: true,
   // 分頁
   pagination: {
-    el: ".swiper-pagination",
+    el: ".swiper-pagination"
   },
   // 左右箭頭
   navigation: {
     nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
+    prevEl: ".swiper-button-prev"
   },
   // 滾動條
   scrollbar: {
     el: ".swiper-scrollbar",
-    draggable: true,
-  },
+    draggable: true
+  }
 });
+//# sourceMappingURL=all.js.map
