48b330598b7a3d97fd7155b1da9415b666652d88
TzuHuang Yen <yan0912@hotmail.com>
2022-09-02 18:46:33 +0200

    enrol

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -13,25 +13,3 @@ const elem = document.querySelector('input[name="datepicker"]');
 const datepicker = new Datepicker(elem, {
   autohide: true,
 });
-
-//index footer swiper
-// <!-- Initialize Swiper -->
-// Swiper
-const swiper = new Swiper(".swiper", {
-  // Optional parameters
-  effect: "fade",
-  fadeEffect: {
-    crossFade: true,
-  },
-
-  // If we need pagination
-  pagination: {
-    el: ".swiper-pagination",
-  },
-
-  // Navigation arrows
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-});
